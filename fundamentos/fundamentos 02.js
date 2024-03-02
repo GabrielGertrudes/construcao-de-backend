a6047bbfcd2701ae3c3efc477bdb9dbc15929266
GabrielGertrudes <gabrielgertrudes151@gmail.com>
2024-03-01 21:52:15 -0300

    fundamentos

---
+++ fundamentos/fundamentos 02.js
@@ -0,0 +1,9 @@
+var nome = "Gabriel"
+// este código é um comentário de uma linha ATALHO: CTRL + ;
+
+/*
+ este código é um comentário de varias linas 
+*/
+
+// imprimi no terminal alguma informação
+console.log("Olá", nome)
---
+++ fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+// criando variaveis com var
+var nomeCompleto
+
+// atribuição na variavel nomeCompleto (=)
+nomeCompleto = "Gabriel Gertrudes"
+
+// console.log("Olá ", nomeCompleto)
+
+var primeiroNome = "Gabriel"
+var nomeDoMeio = "Gertrudes"
+var ultimoNome = "Oliveira"
+var idade = 19
+
+// console.log(priemiroNome, nomeDoMeio. ultimoNome)
+
+console.log("NOME: ", primeiroNome + " " + nomeDoMeio + " " + ultimoNome)
+console.log("Idade: " + idade)
+
+// criando variaveis com const
+const idade = 19
+
+// criando variaveis com let
+let priemiroNome = "Gabriel"
